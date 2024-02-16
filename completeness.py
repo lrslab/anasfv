6ec74c07fc25da9caa6cd0054500032b1d4cea21
like <taxoroger@gmail.com>
2024-02-16 18:52:43 +0800

    Initial commit

---
+++ completeness.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+from Bio import SeqIO
+import os
+import subprocess
+import pandas as pd
+import multiprocessing
+
+
+def cal_BUSCO(consensus_use, prodigal_file, with_MGF=True):
+    consensusfile = f'./consensus_ffn/type{consensus_use}_consensus'
+    df_querys = pd.DataFrame(
+        columns=['name', 'Subject_id', 'identity', 'alignment_length', 'mismatches', 'gap_openings', 'q.start', 'q.end',
+                 's.start', 's.end', 'e-value', 'bit_score'])
+    total_genes = [i.id for i in list(SeqIO.parse(consensusfile + '.ffn', "fasta"))]
+    if not with_MGF:
+        total_genes = [i for i in total_genes if 'MGF' not in i]
+
+    total = len(total_genes)
+    for record in prodigal_file:
+        try:
+            df_query = pd.read_csv(f'./type{consensus_use}_blast/{record.id}.blast', sep='\t', header=None)
+        except:
+            pass
+        else:
+            df_query.rename(columns={0: 'name', 1: 'Subject_id', 2: 'identity', 3: 'alignment_length', 4: 'mismatches',
+                                     5: 'gap_openings', 6: 'q.start', 7: 'q.end', 8: 's.start', 9: 's.end',
+                                     10: 'e-value', 11: 'bit_score'}, inplace=True)
+            df_querys = pd.concat([df_querys, df_query.loc[:0]])
+
+        l_Complete = []
+        l_Fragmented = []
+        nMissing = 0
+
+    df_querys = df_querys.reset_index()
+    for i in range(df_querys.shape[0]):
+        if not with_MGF and 'MGF' in df_querys['Subject_id'][i]:
+            pass
+        elif df_querys['identity'][i] >= 90 and df_querys['alignment_length'][i] / (
+                df_querys['s.end'][i] - df_querys['s.start'][i] + 1) > 0.9:
+            l_Complete.append(df_querys['Subject_id'][i])
+        elif df_querys['identity'][i] >= 30:
+            l_Fragmented.append(df_querys['Subject_id'][i])
+
+    l_Fragmented = list(set(l_Fragmented) - set(l_Complete))
+    nComplete = len(set(l_Complete))
+    nDuplicate = len(l_Complete) - nComplete
+    nFragmented = len(set(l_Fragmented))
+    nMissing = total - nComplete - nFragmented
+    complete_percentage = round((nComplete / total) * 100, 2)
+    duplicate_percentage = round((nDuplicate / total) * 100, 2)
+    fragmented_percentage = round((nFragmented / total) * 100, 2)
+    missing_percentage = round((nMissing / total) * 100, 2)
+
+    busco = f'C:{complete_percentage}%[D:{duplicate_percentage}%],F:{fragmented_percentage}%,M:{missing_percentage}%,n:{total}'
+    return busco
+
+def run_blast(strain, consensus_use):
+    file = f'./prodigal_result/{strain}.fna'
+    prodigal_file = list(SeqIO.parse(file, "fasta"))
+    consensusfile = f'./consensus_ffn/type{consensus_use}_consensus'
+    for record in prodigal_file:
+        SeqIO.write(record, f"./type{consensus_use}_blast/{record.id}.fasta", "fasta")
+        os.system(f"blastn -query ./type{consensus_use}_blast/{record.id}.fasta -out ./type{consensus_use}_blast/{record.id}.blast -db {consensusfile} -outfmt 6 -evalue 1e-5 -num_threads 12")
+
+
+if __name__ =='__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='The consensus cds of genotype II is used by default.')
+    parser.add_argument('consensus', type=str, default='II', help='I or II')
+    args = parser.parse_args()
+    consensus_use = args.consensus
+
+    single_fasta_list = [i[:-6] for i in os.listdir("./single_fasta") if i.endswith('.fasta')]
+    os.system('rm -rf prodigal_result')
+    os.makedirs('prodigal_result')
+
+    for single_fasta in single_fasta_list:
+        prodigal_cmd = f'prodigal -i ./single_fasta/{single_fasta}.fasta -o ./prodigal_result/{single_fasta}.gff -f gff -a ./prodigal_result/{single_fasta}.faa -d ./prodigal_result/{single_fasta}.fna'
+        os.system(prodigal_cmd)
+
+    try:
+        os.makedirs(f'type{consensus_use}_blast')
+    except FileExistsError:
+        pass
+
+    p = multiprocessing.Pool(60)
+    for strain in [i[:-4] for i in os.listdir('./prodigal_result') if i.endswith(".fna")]:
+        p.apply_async(run_blast, args=(strain, consensus_use))
+    p.close()
+    p.join()
+
+    print(f'accession_ID\tsize\twith_MGF\twithout_MGF')
+    for strain in [i[:-4] for i in os.listdir('./prodigal_result') if i.endswith(".fna")]:
+        file = f'./prodigal_result/{strain}.fna'
+        prodigal_file = list(SeqIO.parse(file, "fasta"))
+        size = len(SeqIO.read(f'./single_fasta/{strain}.fasta', "fasta"))
+        busco = cal_BUSCO(consensus_use, prodigal_file)
+        busco_without_MGF = cal_BUSCO(consensus_use, prodigal_file, with_MGF=False)
+        print(f'{strain}\t{size}\t{len(prodigal_file)}\t{busco}\t{busco_without_MGF}')
---
+++ readme.md
@@ -0,0 +1,85 @@
+
+The **anasfv** project focuses on analyzing nanopore-sequenced data of PCR-amplified African Swine Fever Virus (ASFV). The workflow accepts fastq files as input and produces three main outputs: an assembled genome, a genome completeness evaluation similar to BUSCO, and a phylogenetic tree.
+
+## Requirements:
+
+1. python: 3.11 (tested). Most Python 3 versions should work.
+2. Software versions tested:
+	 \- Biopython: 1.8.1
+	 \- Medaka: 1.11.3
+ 	 \- Samtools: 1.17
+  	 \- BEDTools: 2.26.0
+  	 \- Minimap2: 2.17-r941
+  	 \- NanoFilt: 2.8.0
+  	 \- Homopolish: 0.4.1
+  	 \- blast: 2.12.0
+  	 \- MUSCLE: 5.1
+  	 
+ 
+Install requirements:
+```
+pip install biopython
+conda install -c bioconda medaka
+conda install -c bioconda samtools
+conda install -c bioconda bedtools
+conda install -c bioconda minimap2
+conda install -c bioconda nanofilt 
+conda install -c conda-forge -c bioconda homopolish=0.4.1=pyhdfd78af_1
+conda install -c bioconda blast
+conda install -c bioconda muscle
+```
+## Workflow Example:
+
+1. Download all ASFV genomes from NCBI to the "./single_fasta" directory
+```
+download_asfv_genome.py
+```
+2. Merge asfv genome files to one file
+( If you already have assembled genome, you can skip steps 2-8 )
+```
+cat single_fasta/*.fasta > allde.fasta
+```
+3. Trimming
+```
+NanoFilt all.fastq -q 10 -l 1000 --maxlength 200000 --headcrop 50 > all_trimmed.fq
+```
+4. Find nearest genome as ref
+```
+find_near_ref.py -r allde.fasta -f all_trimmed.fq > near.fasta
+```
+5. Use near.fasta as ref to generate sam file
+```
+minimap2 -a near.fasta ./all_trimmed.fq > all-alignment.sam
+```
+6. Generate consensus file
+```
+samtools view -b -F 4 all-alignment.sam > all-alignment.bam
+samtools sort -@ 8 -o all-sorted_alignment.bam all-alignment.bam
+samtools consensus -f fasta all-sorted_alignment.bam -o all-assembled.fa
+```
+7. Polish with medaka
+```
+medaka_consensus -i all_trimmed.fq -d all-assembled.fa -o all-assembly_medaka_result -m r941_min_fast_g303 -t 2 > medaka.log
+```
+8. Polish with homopolish
+( get a final genome)
+```
+homopolish polish -a ./all-assembly_medaka_result/consensus.fasta -l ./near.fasta -m R9.4.pkl -o homopolish-output
+```
+9. Rename final_genome file and move it to the "./single_fasta" directory.
+```
+cp ./homopolish_output/consensus_homopolished.fasta ./single_fasta/strain_name.fasta
+sed -i '1s/.*/>strain_name/' ./single_fasta/strain_name.fasta
+``` 
+10. Genome completeness evaluation
+```
+completeness.py II > completeness.tsv
+```
+11. Get aligenments for uDance
+( find cds in all genome files from "./single_fasta" and get a "./aligenments" directory as input for uDance )
+```
+get_cds_alignments.py
+```
+12. Build a tree using uDance
+Perform a tree construction in de-novo mode and an iterative tree construction in tree mode. For the configuration used, see "./uDance_config_example"
+Refer to [uDance](https://github.com/balabanmetin/uDance)
