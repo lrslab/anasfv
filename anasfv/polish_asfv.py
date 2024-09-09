e283e5966dc2f543b5796c229f47adbbe392a1ed
like <52870888+nimua@users.noreply.github.com>
2024-09-09 16:52:08 +0800

    Update polish_asfv.py

--- anasfv/polish_asfv.py
+++ anasfv/polish_asfv.py
@@ -33,8 +33,8 @@ if __name__=="__main__":
     input = args.input
     ref = args.ref
     homopolish_model = args.model
-        
-    command = f'homopolish polish -a {input} -l {ref} -m {homopolish_model} -o {input}-homopolish-output'
+    
+    command = f'homopolish polish -a {input} -l {ref} -m {homopolish_model} -o {os.path.basename(input)}-homopolish-output'
     conda_env = 'homopolish'
     execute_command_in_conda_env(conda_env, command)
     
